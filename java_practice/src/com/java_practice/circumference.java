96efca0f101152bb40cedb0cec1862620dab55b3
Datta8668 <dattatraybhaganagare3571@gmail.com>
2025-06-29 21:29:15 +0530

    fifth

---
+++ java_practice/src/com/java_practice/circumference.java
@@ -0,0 +1,12 @@
+package com.java_practice;
+import java.util.*;
+public class circumference {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please Enter the Radius: ");
+		double radius = sc.nextDouble();
+		double circum;
+		circum = 2*Math.PI*radius;
+		System.out.println("The circumference of circle is: " + circum);
+	}
+}
