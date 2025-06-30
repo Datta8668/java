fede29f1d336818a58fdea96b7d4f921f71ffcde
Datta8668 <dattatraybhaganagare3571@gmail.com>
2025-06-30 21:42:44 +0530

    update

--- java_practice/src/com/java_practice/Greater.java
+++ java_practice/src/com/java_practice/Greater.java
@@ -7,8 +7,15 @@ public class Greater {
 		double num1 = fd.nextDouble();
 		System.out.print("Please Enter the second number: ");
 		double num2 = fd.nextDouble();
+		System.out.print("Please Enter the third number");
+		double num3 = fd.nextDouble();
 		if(num1 >= num2) {
+			if(num1 >= num3) {
 			System.out.println(num1+" is greater.");
+			}
+			else {
+				System.out.println(num3+ " is greater.");
+			}
 		}
 		else {
 			System.out.println(num2+" is greater.");
