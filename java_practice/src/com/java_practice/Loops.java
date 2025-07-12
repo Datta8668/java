a01e10f51503f40e15abc70718a038a3c20d2dd9
Datta8668 <dattatraybhaganagare3571@gmail.com>
2025-07-12 17:17:14 +0530

    Loops

---
+++ java_practice/src/com/java_practice/Loops.java
@@ -0,0 +1,171 @@
+package com.java_practice;
+
+public class Loops {
+
+	public static void main(String[] args) {
+
+	
+		
+
+//		for (int i=1; i<=3; i++) {				
+//			for (int j=1; j<=3; j++) {		
+//			System.out.print(" *");
+//			}
+//			System.out.println(" ");
+//		}
+		
+// 	Output:
+//  	* * * 
+// 		* * * 
+// 		* * * 
+
+
+
+		
+//		for (int i=1; i<=4; i++) {
+//			for (int j=1; j<=5; j++) {
+//				if (i==1 || j==1 || i==4 || j==5 ) {
+//					System.out.print("*");
+//				}
+//				else {
+//				System.out.print(" ");
+//				}
+//			}
+//			System.out.println();
+//		}	
+		
+//	Output:	
+//		*****
+//		*   *
+//		*   *
+//		*****
+
+
+
+		
+//		for(int i=1; i<=4; i++) {
+//			for(int j=1; j<=i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println(" ");
+//		}
+			
+//	Output:
+//		* 
+//		** 
+//		*** 
+//		**** 
+
+		
+		
+		
+//		for(int i=4; i>=1; i--) {
+//			for(int j=1; j<=i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println(" ");
+//		}		
+		
+//	Output:	
+//		**** 
+//		*** 
+//		** 
+//		* 
+	
+
+		
+		
+//		for(int i=1; i<=4; i++) {
+//			for(int j=1; j<=4-i; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int k=1; k<=i; k++) {
+//				System.out.print("*");
+//			}
+//		System.out.println();
+//	}
+		
+//	Output:
+//		    *
+//		   **
+//		  ***
+//		 ****
+
+
+		
+		
+//		for(int i=1; i<=5; i++) {
+//		for(int j=1; j<=i; j++) {
+//			System.out.print(j+" ");
+//		}
+//		System.out.println(" ");
+//	}		
+
+//	Output:
+//		1  
+//		1 2  
+//		1 2 3  
+//		1 2 3 4  
+//		1 2 3 4 5 
+	
+	
+		
+		
+//		for(int i=1; i<=5; i++) {
+//		for(int j=1; j<=5-i+1; j++) {
+//			System.out.print(j+" ");
+//		}
+//		System.out.println(" ");
+//	}				
+		
+//	Output:
+//		1 2 3 4 5  
+//		1 2 3 4  
+//		1 2 3  
+//		1 2  
+//		1  
+		
+		
+		
+		
+//		int n=1;
+//		for(int i=1; i<=4; i++) {
+//			for(int j=1; j<=i; j++) {
+//				System.out.print(n+" ");
+//				n++;
+//			}
+//			System.out.println(" ");
+//		}
+		
+//	Output:	
+//		1  
+//		2 3  
+//		4 5 6  
+//		7 8 9 10 
+
+		
+		
+		
+//		for(int i=1; i<=5; i++) {
+//			for(int j=1; j<=i; j++) {
+//				int sum = i+j;
+//				if (sum % 2 == 0) {
+//					System.out.print(" 1");
+//				}
+//				else {
+//					System.out.print(" 0");
+//				}
+//			}
+//			System.out.println(" ");
+//		}
+		
+//	Output:
+//		 1 
+//		 0 1 
+//		 1 0 1 
+//		 0 1 0 1 
+//		 1 0 1 0 1
+		
+		
+	}
+}
