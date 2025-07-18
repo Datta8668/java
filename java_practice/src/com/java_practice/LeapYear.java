ac76cbb2934bd769214972949f68dc01d42fd915
Datta8668 <dattatraybhaganagare3571@gmail.com>
2025-07-18 23:59:53 +0530

    java

--- java_practice/src/com/java_practice/LeapYear.java
+++ java_practice/src/com/java_practice/LeapYear.java
@@ -6,11 +6,14 @@ public class LeapYear {
 	public static void main(String[] args) {
 		
 		Scanner sd = new Scanner (System.in);
+		
 		System.out.println("Please Enter the year.");
+		
 		int year = sd.nextInt();
 		
 		if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
 			System.out.println(year+" is leaf year.");
+	
 		}
 		else {
 			System.out.println(year+ " is not leaf year.");
