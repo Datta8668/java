ac69731675df17902ab3a01f0700daa26b0d463d
Datta8668 <dattatraybhaganagare3571@gmail.com>
2025-07-19 00:00:20 +0530

    java

--- java_practice/src/com/java_practice/circumference.java
+++ java_practice/src/com/java_practice/circumference.java
@@ -1,7 +1,10 @@
 package com.java_practice;
 import java.util.*;
+
 public class circumference {
+	
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Please Enter the Radius: ");
 		double radius = sc.nextDouble();
