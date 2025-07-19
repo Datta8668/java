ceda259ba44bdb2654b60ad189bce33c4f327064
Datta8668 <dattatraybhaganagare3571@gmail.com>
2025-07-19 22:21:22 +0530

    java

---
+++ java_practice/src/com/java_practice/LastDigit.java
@@ -0,0 +1,22 @@
+package com.java_practice;
+
+import java.util.Scanner;
+
+public class LastDigit {
+
+	public static void main(String[] args) {
+
+		Scanner sd = new Scanner (System.in);
+		
+		System.out.println("Enter the number: ");
+		
+		int n = sd.nextInt();
+		
+		int d = n % 10;
+		
+		System.out.println("The last digit of number is: "+d);
+		
+
+	}
+
+}
